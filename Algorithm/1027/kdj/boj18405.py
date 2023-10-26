83976aa3f9d7442197d251a997bf7537f7f08cea
childevnote <daniel011125@gmail.com>
2023-10-26 18:13:45 +0900

    김동진 : 18405 문제풀이

---
+++ Algorithm/1027/kdj/boj18405.py
@@ -0,0 +1,26 @@
+import copy
+
+n, k = map(int, input().split())
+tube = [list(map(int, input().split())) for _ in range(n)]
+s, x, y = map(int, input().split())
+
+test_tube = copy.deepcopy(tube)
+dx=[0,0,-1,1]
+dy=[-1,1,0,0]
+
+for _ in range(s):
+  num = 1
+  while num <= k:
+    for tx in range(len(tube)):
+      for ty in range(len(tube[tx])):
+        if tube[tx][ty] == num:
+          for i in range(4):
+            nx = tx + dx[i]
+            ny = ty + dy[i]
+            if nx>=0 and nx<n and ny>=0 and ny<n:
+              if test_tube[nx][ny] == 0:
+                test_tube[nx][ny] = tube[tx][ty]
+    tube = copy.deepcopy(test_tube)
+    num += 1
+
+print(tube[x-1][y-1])
